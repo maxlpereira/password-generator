4e623cf2ed1a588f0dd5375aace3d1ee1ff725c2
Maxwell Lima Pereira <121093303+maxlpereira@users.noreply.github.com>
2022-12-30 17:52:16 +0000

    Add files via upload

---
+++ passgenerator.py
@@ -0,0 +1,28 @@
+import string
+import random
+
+numb = list(range(0,10))
+for i in numb:
+    numb[i] = str(i)
+
+alpha_l = list(string.ascii_lowercase)
+
+alpha_h = list(string.ascii_uppercase)
+
+
+library = alpha_l+alpha_h+numb
+
+gen = ''
+
+count = 0
+for p in range(16):
+    count += 1
+    gen += random.choice(library)
+    if p == 15:
+        continue  
+    elif count == 4:
+        gen += '-'
+        count = 0
+
+print(gen)
+
